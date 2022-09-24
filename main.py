8268e04ba80b82e1fdfcce1f0b95097a18eaa27c
Abdul Muqeet <theabdulmuqeet1@gmail.com>
2022-09-24 12:58:35 +0500

    Update main.py

--- main.py
+++ main.py
@@ -1,205 +1,275 @@
+import os
+from time import sleep
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.common.exceptions import WebDriverException
-import os
+download_directory = os.path.join(os.path.expandvars("%userprofile%"), "Downloads")
+link_file = "resume_download.txt"
 
-try:
-    startingEpisodeNumber = 0
-    episodeDownloadable = 0
-    endingEpisodeNumber = 0
-    episodesToDownload = []
-    names_of_episodesToDownload = []
-    episodes_not_downloaded = []
-    episodes_downloaded = []
-    episodes = []
-    count = 0
-    totalEpisodes = 0
-    choice = 0
-    tempBool = 0
-    all_downloaded = 0
-    browser = 0
-    _404 = 0
-
+def which_browser():
     # Selection of the browser
-    while (browser < 1 or browser > 2):
-        os.system("cls")
+    browser = 0
+    while (True):
         print("Select the browser")
-        browser = int(input("""
-            1. Chrome browser
-            2. Microsoft Edge
-            Selection: """))
+        try:
+            browser = int(input("""
+                1. Chrome browser
+                2. Microsoft Edge
+                Selection: """))
+        except ValueError:
+            pass
 
+        if(not(browser == 1 or browser == 2)):
+            os.system("cls")
+            print("Invalid input")
+        else:
+            return browser
+
+def which_method():
     # Selecting download method
-    while (not choice >= 1 or not choice <= 3):
-        if (tempBool > 0):
-            print("==> Please enter the right options")
-        print("\nChoose download method:")
-        choice = int(input("""
-            1. All episodes
-            2. Range of episodes
-            3. Some episodes
-            Selection: """))
-        tempBool += 1
+    choice = 0
+    print()
+    while (True):
+        try:
+            print("Choose download method:")
+            choice = int(input("""
+                1. All episodes
+                2. Range of episodes
+                3. Some episodes
+                Selection: """))
+        except ValueError:
+            pass
+        
+        if(not(choice == 1 or choice == 2 or choice == 3)):
+            os.system("cls")
+            print("\nInvalid Choice")
+        else:
+            return choice
 
-    print("\nEnter data required to start the download")
-    print("------------------------------------------", end="\n")
+def get_link():
+    archive_base = "episodes.animeflix.org.in/archives"
+    while(True):
+        archive_link = input("Enter archive link: ")
+        if(not(archive_base in archive_link)):
+            os.system("cls")
+            print("Invalid link")
+        else:
+            return archive_link
 
-
-    mainLink = input("Enter archive link: ")
-    # download_directory = input("Enter download directory (the download directory your browser have): ")
-    download_directory = os.path.join(os.path.expandvars("%userprofile%"), "Downloads")
-
-
-    if choice == 2:
-        startingEpisodeNumber = int(input("Enter starting episode number: "))
-        endingEpisodeNumber = int(input("Enter ending episode number: "))
-        episodeDownloadable = (endingEpisodeNumber - startingEpisodeNumber) + 1
-    elif choice == 3:
-        print("Enter episode numbers (press 0 to exit)")
+def which_episodes(episodeChoice):
+    episodes = []
+    if(episodeChoice == 2):
+        while(True):
+            try:
+                episodes.append(int(input("Enter starting episode number: ")))
+                if(episodes[0] > 0):
+                    break
+            except ValueError:
+                print("Invalid starting episode number")
+        while(True):
+            try:
+                temp = int(input("Enter ending episode number (enter '9876' to select the last episode): "));
+                if(temp > 0 and temp > episodes[0]):
+                    episodes.append(temp)
+                    break
+                else:
+                    print(f"Ending episode must be greater than starting episode(starting episode: {episodes[0]})")
+                    print('----------------------------------------------------------------------------')
+            except ValueError:
+                print("Invalid ending episode number")
+        if temp == 9876:
+            pass
+        else:
+            print(f"{(episodes[1] - episodes[0])+1} episodes will be downloaded (ep {episodes[0]} - ep {episodes[1]})")
+    elif(episodeChoice ==3):
+        print("Enter episode numbers (enter 0 to exit)")
         temp = -1
-        while (temp != 0):
-            temp = int(input("Episode "))
-            if (int(temp) and temp != 0):
-                episodesToDownload.append(temp)
-        episodesToDownload.sort()
-        print("Episode(s) ", episodesToDownload, " will be downloaded")
-        episodeDownloadable = len(episodesToDownload)
+        while(True):
+            try:
+                temp = int(input("Episode "))
+                if(temp == 0):
+                    break
+                episodes.append(temp)
+            except ValueError:
+                pass
+        episodes.sort()
+        episodes = list(set(episodes))
+        print("Episode(s) ", episodes, " will be downloaded")
+    return episodes
+            
+def are_there_links():
+    if link_file in os.listdir():
+        with open(link_file, 'r') as resume_process:
+            resume_links = resume_process.readlines()
+            if resume_links[0].startswith('https://episodes.animeflix.org.in'):
+                print("================================================================================================\nBackup file found!")
+                resume_choice = input("If you wish to complete/resume your previous incomplete download, enter 'y': ")
+                if resume_choice == 'y':
+                    return resume_links
+    return 0
+
+def driver_process(browser_choice, required_episodes_numbers, method, resume_links = 0):
+    if(resume_links == 0):
+        mainLink = get_link()
+    else:
+        mainLink = resume_links[0]
 
     # Starting the driver
-    print("===================================================")
+    print("===========================================================================")
     print("The browser is now starting...")
 
-    # Adding arguments
-    if browser == 2:
+    if browser_choice == 2:
         options = webdriver.EdgeOptions()
-    else:
+    elif browser_choice == 1:
         options = webdriver.ChromeOptions()
 
     options.add_argument('--log-level=3')
     options.add_experimental_option('excludeSwitches', ['enable-logging'])
 
     # Opening the browser
-    if browser == 2:
+    if browser_choice == 2:
         driver = webdriver.Edge(options=options)
     else:
         driver = webdriver.Chrome(options=options)
     driver.set_window_position(0, 0, windowHandle='current')
     driver.get(mainLink)
-    print("Please wait! Counting the episodes...")
-    elements = driver.find_elements(By.XPATH, '//h3/a')
+    
+    links_to_all_episodes = []
+    if resume_links == 0:
+        # Couting the anchor tags on the page
+        print("Please wait! Counting the episodes...")
+        elements = driver.find_elements(By.XPATH, '//h3/a')
 
-    # counting total episodes
-    for element in elements:
-        episodes.append(element.get_attribute('href'))
-        totalEpisodes += 1
+        # counting total episodes and saving the links
+        total_anchor_tags = 0
 
-    if choice == 1:
-        startingEpisodeNumber = 1
-        endingEpisodeNumber = totalEpisodes
-        episodeDownloadable = totalEpisodes
-    print("Total no. episodes on site : ", totalEpisodes)
-    print("No. of episodes to download: ", episodeDownloadable)
-    print("===================================================")
+        with open(link_file, 'w') as writing_episode_links:
+            for element in elements:
+                total_anchor_tags += 1
+                if method == 1:
+                    writing_episode_links.write(f"{element.get_attribute('href')}\n")
+                    links_to_all_episodes.append(f"{element.get_attribute('href')}\n")
+                elif(method == 2):
+                    if(total_anchor_tags >= required_episodes_numbers[0] and total_anchor_tags <= required_episodes_numbers[1]):
+                        writing_episode_links.write(f"{element.get_attribute('href')}\n")
+                        links_to_all_episodes.append(f"{element.get_attribute('href')}\n")
+                elif(method == 3):
+                    for episode_number in required_episodes_numbers:
+                        if(total_anchor_tags == episode_number):
+                            writing_episode_links.write(f"{element.get_attribute('href')}\n")            
+                            links_to_all_episodes.append(f"{element.get_attribute('href')}\n")
+        if method == 1:
+            print("No. of episodes to download: ", total_anchor_tags)
+        elif method == 2:
+            temp = required_episodes_numbers[1]
+            start = required_episodes_numbers[0]
+            end = len(links_to_all_episodes) + start
+            required_episodes_numbers.clear()
+            for i in range(start, end):
+                required_episodes_numbers.append(i)
+            if temp == 9876:
+                print(f"{len(required_episodes_numbers)} episodes will be downloaded (ep {required_episodes_numbers[0]} - ep {required_episodes_numbers[-1]})")
 
+        print("Total no. episodes on site : ", total_anchor_tags)
+        print("===========================================================================")
+    else:
+        links_to_all_episodes = resume_links
 
-    # downloading episodes
-    print("Downloads are now started")
+    # download starts here
+    print("Downloads are now starting...")
     print("==> \tPrompt: if any episode fails to download, re-download it")
-    count = 0
-    currentEpisodeNumber = startingEpisodeNumber
-    for episode in episodes:
-        count += 1
-        if choice == 1 or choice == 2:
-            if (count >= startingEpisodeNumber and count <= endingEpisodeNumber):
-                driver.get(episode)
-                if (driver.find_element(By.TAG_NAME, 'h3').text == "404! Page Not Found"):
-                    print(
-                        f"Error: Episode {count} cannot be downloaded because it has been removed from the server")
-                    _404 += 1
-                    continue
-                direct_mirror = driver.find_element(
-                    By.CLASS_NAME, 'btn-outline-success')
-                driver.get(direct_mirror.get_attribute('href'))
+    episodes_processed = []
+    for (episode_link, episode_number)  in zip(links_to_all_episodes, required_episodes_numbers):
+        driver.get(episode_link)
+        if (driver.find_element(By.TAG_NAME, 'h3').text == "404! Page Not Found"):
+            print(f"Error: An episode was not available at website's server")
+            continue
+        
+        direct_mirror = driver.find_element(By.CLASS_NAME, 'btn-outline-success')
+        driver.get(direct_mirror.get_attribute('href'))
+        refresh_mirror = driver.find_element(By.ID, 'checkfile')
+        refresh_mirror.click()
 
-                refresh_mirror = driver.find_element(By.ID, 'checkfile')
-                refresh_mirror.click()
+        download_direct = driver.find_element(By.CLASS_NAME, 'btn-success')
+        driver.get(download_direct.get_attribute('href'))
 
-                download_direct = driver.find_element(By.CLASS_NAME, 'btn-success')
-                driver.get(download_direct.get_attribute('href'))
+        current_episode_name = driver.find_element(By.CLASS_NAME, 'card-header').text
+        episodes_processed.append(current_episode_name)
+        download_now = driver.find_element(By.NAME, 'download')
+        if resume_links == 0:
+            print(f"Episode {episode_number} is downloading...")
+        else:
+            print(f"Episode '{current_episode_name}' is downloading...")
 
-                names_of_episodesToDownload.append(
-                    driver.find_element(By.CLASS_NAME, 'card-header').text)
-                download_now = driver.find_element(By.NAME, 'download')
-                print(f"Episode {currentEpisodeNumber} is downloading.")
-                download_now.click()
-                currentEpisodeNumber += 1
-
-        elif choice == 3:
-            for i in episodesToDownload:
-                if (count == i):
-                    if count <= totalEpisodes:
-                        currentEpisodeNumber = i
-                        driver.get(episode)
-                        direct_mirror = driver.find_element(
-                            By.CLASS_NAME, 'btn-outline-success')
-                        driver.get(direct_mirror.get_attribute('href'))
-
-                        refresh_mirror = driver.find_element(By.ID, 'checkfile')
-                        refresh_mirror.click()
-
-                        download_direct = driver.find_element(
-                            By.CLASS_NAME, 'btn-success')
-                        driver.get(download_direct.get_attribute('href'))
-
-                        names_of_episodesToDownload.append(
-                            driver.find_element(By.CLASS_NAME, 'card-header').text)
-                        download_now = driver.find_element(By.NAME, 'download')
-                        print(f"Episode {currentEpisodeNumber} is downloading.")
-                        download_now.click()
-
-                    else:
-                        print("Episode ", i , " not found")
+        download_now.click()
 
     print("\nPlease wait, downloading is in progress...")
-    print("Check the browser for download information")
-    print("Do not close any tab or the browser")
+    print("Check the browser for download progress")
+    print("Do not close the browser or any tab")
+    wait()
+    driver.close()
+    return episodes_processed
+
+def wait():
+    sleep(5)
     dl_wait = True
     while dl_wait:
         dl_wait = False
         for fname in os.listdir(download_directory):
             if fname.endswith('.crdownload'):
                 dl_wait = True
-    driver.close()
-    os.system("cls")
-    print("\n==============")
-    print("Final result")
-    print("=============")
-    for episode_name in names_of_episodesToDownload:
-        for fname in os.listdir(download_directory):
-            if fname == episode_name:
-                tempFound = 0
-                break
+
+def main_engine():
+    try:
+        os.system("cls")
+        browser = which_browser()
+        resume = are_there_links();
+
+        try:
+            downloaded_episodes = []
+            if resume == 0:
+                choice = which_method()
+                print("\nEnter data required to start the download")
+                print("------------------------------------------", end="\n")                
+
+                # downloading episodes
+                required_episodes = which_episodes(choice)
+                downloaded_episodes = driver_process(browser, required_episodes, choice)
             else:
-                tempFound = 1
-        if(tempFound):
-            episodes_not_downloaded.append(episode_name)
-            all_downloaded += 1
-        else:
-            episodes_downloaded.append(episode_name)
-    if all_downloaded == 0:
-        print("No errors were encountered.")
+                downloaded_episodes = driver_process(browser, [0, 0], 0, resume_links=resume)
 
-    elif not _404 == 0:
-        print("No episodes were found on website's server")
-    else:
-        print("\nRe-download the listed episodes")
-        print("-------------------------------")
-        for episode_name in episodes_not_downloaded:
-            print(f"{episode_name} failed to download")
+            print("===========================================================================")
+            os.system("cls")
+            print("\n==============")
+            print(" Final result")
+            print("==============")
 
-    print("\nFollowing episodes were succesfully downloaded")
-    print("----------------------------------------------")
-    for episode_name in episodes_downloaded:
-        print(episode_name)
-except WebDriverException:
-    print("\nBrowser was unexpectedly closed")
+            episode_links_in_file = []
+            with open(link_file, 'r') as reading_links:
+                episode_links_in_file = reading_links.readlines()
+            
+            # After Download
+            remaining_episode_links = []
+            for i in range(len(required_episodes)):
+                if downloaded_episodes[i] in os.listdir(download_directory):
+                    print(f" OO ==> Episode {required_episodes[i]} successfully downloaded")
+                else:
+                    remaining_episode_links.append(episode_links_in_file[i])
+                    print(f" XX ==> Episode {required_episodes[i]} download failed")
+
+            with open(link_file, 'w') as writing_remaining_links:
+                writing_remaining_links.writelines(remaining_episode_links)
+                
+        except WebDriverException:
+            print("\nBrowser was unexpectedly closed (probably due to bad internet connection)\nAll progress was lost. Please restart the program")
+
+    except KeyboardInterrupt:
+        print("""
+        ===============================
+        Program was exited by the user
+        ===============================
+        """)
+
+if __name__ == "__main__":
+    main_engine()
+    print("If any error occured and you don't know it's solution, feel free to report it as an issue at my github.\nLink to github: https://github.com/TheAbdulMuqeet")
